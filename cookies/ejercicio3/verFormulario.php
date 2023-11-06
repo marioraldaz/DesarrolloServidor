4da1e6e3ed05f007cf20e4939c93c34838e4d61b
marioraldaz <130656068+marioraldaz@users.noreply.github.com>
2023-11-06 10:52:32 +0100

    Redirects to index to make testing better

--- cookies/ejercicio3/verFormulario.php
+++ cookies/ejercicio3/verFormulario.php
@@ -14,8 +14,8 @@
         $idioma=$listaIdiomas[$_COOKIE['idioma']];
         echo <<<FORM
         <div class='container' style=background-color:$_COOKIE[colorFondo] color:$_COOKIE[fontColor]>
-            <form method='POST' action=''>
-                $idioma[idioma] <input type='text' name="idioma"/>
+            <form method='POST' action='index.php'>
+                $idioma[idioma] <input type='text' name="idioma" value=$idioma[idioma]/>
                 $idioma[colorFuente] <input type="color" name="colorFuente"/>
                 $idioma[colorFondo] <input type="color" name="colorFondo"/>
                 <input type='submit' value="$idioma[Enviar]" />
